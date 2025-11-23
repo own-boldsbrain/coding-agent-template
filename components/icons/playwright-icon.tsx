39197fbec7b2130e106eb234d798289c73074921
own-boldsbrain <fernando@yellosolarhub.com>
2025-11-23 11:23:22 -0300

    ok

--- components/icons/playwright-icon.tsx
+++ components/icons/playwright-icon.tsx
@@ -1,7 +1,8 @@
 import type { SVGProps } from 'react'
 
 const PlaywrightIcon = (props: SVGProps<SVGSVGElement>) => (
-  <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" fill="none" {...props}>
+  <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" fill="none" aria-labelledby="playwright-icon-title" {...props}>
+    <title id="playwright-icon-title">Playwright</title>
     <path
       fill="currentColor"
       opacity="0.8"
